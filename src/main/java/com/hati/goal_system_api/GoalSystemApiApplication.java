9c67df62e78199dc7a4e90862b21bf95e5392669
Hatice Taylan <htc_tylnn@hotmail.com>
2026-05-27 14:20:25 +0200

    project setup

---
+++ src/main/java/com/hati/goal_system_api/GoalSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.hati.goal_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hati/goal_system_api/GoalSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hati.goal_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
